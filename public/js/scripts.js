37c6282a56a5f2fa7540a57dc63478952f17beb5
Steven Wanderski <steven@bxcreative.com>
2012-11-15 21:36:47 +0100

    added sidebar royalties

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -49,26 +49,14 @@ $(function(){
 	}
 	
 	/**
-	 * EXAMPLES
+	 * SIDEBAR
 	 */
 	
-	// JS / HTML code switcher
-	$('.code-switch a').click(function(){
-		if(!$(this).hasClass('active')){
-			$('.code-switch a').removeClass('active');
-			$(this).addClass('active');
-			var code = $(this).attr('data-code');
-			$('.code-wrap .active').fadeOut(100, function(){
-				$(this).removeClass('active');
-				$('.code-wrap .code[data-code="' + code + '"]').fadeIn(100).addClass('active');
-			});
-		}
+	$('.btn-donate').click(function() {
+		$('#frm-paypal').submit();
 		return false;
 	});
 	
-	/**
-	 * SIDEBAR
-	 */
 	$('.block-signup form').submit(function() {
 		var email = $('#mce-EMAIL').val();
 		if(!isValidEmailAddress(email)){
@@ -83,3 +71,11 @@ function isValidEmailAddress(emailAddress) {
 	var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
 	return pattern.test(emailAddress);
 };
+
+(function(){
+	var bsa = document.createElement('script');
+	bsa.type = 'text/javascript';
+	bsa.async = true;
+	bsa.src = '//s3.buysellads.com/ac/bsa.js';
+	(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);
+})();
